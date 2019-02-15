5aaef1ce3186c4461cb372ca14eb393e8980a365
Alex Ryndin <alexnryndin@gmail.com>
2019-02-15 12:49:06 +0300

    improve code

--- hw5/lsof.hs
+++ hw5/lsof.hs
@@ -1,7 +1,14 @@
 import System.Directory
+import System.FilePath
 import Control.Monad
+import Control.Exception
 import System.Posix.Files
+import Data.Foldable
+
 import Data.Char
+
+getAbsDirectoryContents :: FilePath -> IO [FilePath]
+getAbsDirectoryContents dir = map (dir </>) <$> getDirectoryContents dir
 
 getProcs :: IO [FilePath]
 getProcs = (filter (\x -> not (any isAlpha x)) <$> (listDirectory "/proc"))
@@ -12,9 +19,18 @@ getProcsFullPaths = (fmap . fmap) ("/proc/" ++) getProcs
 getProcsFDPaths :: IO [FilePath]
 getProcsFDPaths = (fmap . fmap) (++ "/fd") getProcsFullPaths
 
-getFDSLs :: IO [[FilePath]]
-getFDSLs = join (sequence <$> (fmap . fmap) listDirectory  getProcsFDPaths )
+getFDContents :: IO [[FilePath]]
+getFDContents = join (sequence <$> (fmap . fmap) getAbsDirectoryContents getProcsFDPaths )
 
+getFDSLs = join $ traverse (filterM (fmap (not . isDirectory) . getFileStatus)) <$> getFDContents
+
+main :: IO ()
+main = do
+  result <- try getFDSLs :: IO (Either SomeException [[FilePath]])
+  case result of
+    Left ex -> putStrLn "Fuck"
+    Right val -> do
+      sequence_ [putStrLn file | files <- val, file <- files]
 --getSLPaths :: IO [[FilePath]]
 --getSLPaths = do
 --  x <- getProcsFDPaths
@@ -23,4 +39,3 @@ getFDSLs = join (sequence <$> (fmap . fmap) listDirectory  getProcsFDPaths )
 --  y1 <- x1
 --  z1 <- y1
 --  return $ return $ return $ readSymbolicLink (y ++ z1)
---listFDSLs 
